4f6d411c5f3d2453487ed7540654aad16a9aafce
Rodrigo Motoshima <motoshima@aol.com>
2023-04-18 10:23:42 -0300

    Initial commit

---
+++ WebMvcMysql/Data/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CRUD_MYSQL.Models;
+
+namespace CRUD_MYSQL.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options)
+        { }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
---
+++ WebMvcMysql/Program.cs
@@ -0,0 +1,35 @@
+using CRUD_MYSQL.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseSqlServer
+    ("Server=localhost; Database=Docker_DB; User Id=sa; Password=@zjdjpr1991M; TrustServerCertificate=True"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Usuarios}/{action=Index}/{id?}");
+
+app.Run();
